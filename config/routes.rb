e55d1e617e105b9310bf987d41912d4ca5eec967
Emmy NSABIMANA <emmy66418@gmail.com>
2022-12-13 01:30:31 +0200

    Implement sign in before any action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
+  devise_for :users
+  
   root 'home#index'
   
   resources :movies, only: %i[index show]

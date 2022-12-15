4352eeb27e6e06a91dab2510f9fae5c7cc13714b
Emmy NSABIMANA <emmy66418@gmail.com>
2022-12-15 12:09:51 +0200

    Update the view and add category table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @q =  Movie.ransack(params[:q])
-    @movies = @q.result(distinct: true).page(params[:page])
+    @movies = @q.result(distinct: true).page(params[:page]).includes(image_attachment: :blob)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_151834) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_15_100736) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_151834) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string "slug", null: false
     t.integer "sluggable_id", null: false
@@ -61,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_151834) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "category_id"
     t.index ["slug"], name: "index_movies_on_slug", unique: true
   end
 

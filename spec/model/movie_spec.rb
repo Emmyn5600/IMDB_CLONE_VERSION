a979f81420908d81f98bf2a1dd56c32cae3aee87
Emmy NSABIMANA <emmy66418@gmail.com>
2022-12-19 08:32:04 +0200

    Revert ruby version

--- spec/model/movie_spec.rb
+++ spec/model/movie_spec.rb
@@ -16,9 +16,27 @@
 #     have_select('#Action', :options => ['Action', 'Love', 'Holyhood', 'Comedy'])
 #     fill_in 'Movie Title', with: 'Anything'
 #     fill_in 'Description', with: 'Anything'
-#     fill_in 'movie[movie_image]', with: "download.png"
+#     # fill_in image { Rack::Test::UploadedFile.new(Rails.root.join('spec/files/images/download.png'), 'image/png') }
+#     have_attached_file(:movie_image)
+#     # fill_in 'movie[movie_image]', with: "download.png"
 #     click_on 'Create Movie'
 #     expect(page).to have_text('Movie was successfully created.')
 #   end
 
 # end
+
+# In rspec, test if the image has been attached using rspec.
+
+# Path: spec/models/movie_spec.rb
+require 'rails_helper'
+
+RSpec.describe Movie, type: :model do
+    it { should have_category_id(:category_id) }
+    it { should have_title(:title) }
+    it { should have_description(:description) }
+  it { should have_attached_file(:movie_image) }
+  it { should validate_attachment_content_type(:movie_image).
+                allowing('image/png', 'image/gif').
+                rejecting('text/plain', 'text/xml') }
+end
+
